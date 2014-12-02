d000db94ce39a0acbb1f95465df1f7d85d7dba23
samvantran <samvantran@gmail.com>
2014-12-02 17:59:23 -0500

    Add user can now log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,16 @@ class SessionsController < ApplicationController
 
   def create
     response = Instagram.get_access_token(params[:code], :redirect_uri => ENV["IG_CALLBACK"])
-    session[:access_token] = response.access_token    
-    redirect_to instagram_index_path
+    session[:access_token] = response.access_token
+    if session[:access_token]
+      redirect_to instagram_index_path
+    else
+      redirect_to new_instagram_path, :notice => "Sorry, you were not authenticated. Please try again."
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to new_instagram_path
   end
 end
