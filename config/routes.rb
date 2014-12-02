857e8259bff263df5000d655bbca3ae34b4b7002
samvantran <samvantran@gmail.com>
2014-12-02 14:09:05 -0500

    Add simple instagram oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'instagram/index'
+  
+  resources :instagram, only: [:index, :new]
+  get 'auth/:provider/callback' => 'instagram#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
