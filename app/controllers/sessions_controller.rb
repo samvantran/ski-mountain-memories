15d7ac7d80417565b5b222e98450ed65eda682df
samvantran <samvantran@gmail.com>
2014-12-04 10:46:17 -0500

    add sweet background image and snow effect
    fix photo_i error
    
    fix sessions controller error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,25 +26,26 @@ class SessionsController < ApplicationController
   end
 
   def sub_callback
-# Instagram.create_subscription("user", "https://ski-mountain-memories.herokuapp.com/sessions/sub_callback")
-# Instagram.create_subscription("tag", "http://localhost:3000/sessions/sub_callback", options: {object_id: "snow"})
-#also delete subscriptins sometime
+  # Instagram.create_subscription("user", "https://ski-mountain-memories.herokuapp.com/sessions/sub_callback")
+  # Instagram.create_subscription("tag", "http://localhost:3000/sessions/sub_callback", options: {object_id: "snow"})
+  #also delete subscriptins sometime
 
     if params["hub.challenge"]
       render :text => params["hub.challenge"]
     else
-      current_tag=params[:_json][0][:object_id]#the tag that just popped
-      response=Instagram.tag_recent_media(current_tag)
-      Media.create(trip_id: XX, 
-        photo: response[:data][photo_i][:type],
-        time_taken: response[:data][photo_i][:created_time], 
-        thumbnail_url: response[:data][photo_i][:images][:thumbnail][:url], 
-        standard_url: response[:data][photo_i][:images][:standard_resolution][:url], 
-        caption: response[:data][photo_i][:caption][:text])
+      current_tag = params[:_json][0][:object_id]         #the tag that just popped
+      
+      response = Instagram.tag_recent_media(current_tag)
+      Media.create( trip_id:        XX, # need to add trip id
+                    photo:          response[:data][0][:type],
+                    time_taken:     response[:data][0][:created_time], 
+                    thumbnail_url:  response[:data][0][:images][:thumbnail][:url], 
+                    standard_url:   response[:data][0][:images][:standard_resolution][:url], 
+                    caption:        response[:data][0][:caption][:text])
     end
 
   end
 
 
-end#class end
+end   #class end
 
