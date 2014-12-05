6c0951d6e29579e184ab55f676edcb13e96d8028
Jimmy Kuruvilla <jimmyjk@gmail.com>
2014-12-05 14:08:09 -0500

    pushed to heroku, mapping new hashtags works! git push

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,6 @@
 class Trip < ActiveRecord::Base
   has_and_belongs_to_many :users
+  validates_uniqueness_of :hashtag
   belongs_to :mountain
   has_many :visuals
   accepts_nested_attributes_for :users
