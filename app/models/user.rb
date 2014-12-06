a4cb8a151a4b2df1b72381b548a53b2c62e229f6
Jimmy Kuruvilla <jimmyjk@gmail.com>
2014-12-06 18:56:26 -0500

    added user show page functionality (shareable link ha ha), and 1. added destroy_subs to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :trips
   has_many :visuals, through: :trips
+
+#should this belong to a trip?
+#this is just so admins can delete everyything. 
+  def self.destroy_subs(tag_id=nil)
+    if tag_id==nil
+      Instagram.delete_subscription(object: "all")#all subs user, locations, tags etc. 
+    else
+      Instagram.delete_subscription(object: "tag", object_id: tag_id)
+  end
+end
 end
