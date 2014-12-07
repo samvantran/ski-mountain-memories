cd806a4c80df07d176d1bd52f7814f92b8c8b948
blakedeboer <deboer.blake@gmail.com>
2014-12-07 11:39:54 -0500

    cleaned up js map files

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -3,43 +3,60 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(function() {
+  
   function initialize() {
-    // var mapCanvas = document.getElementById('map-canvas');
-    // var lat = document.getElementById('map-canvas').getAttribute('data-lat'); 
-    // var lng = document.getElementById('map-canvas').getAttribute('data-lng');
-    // var zoom_level = document.getElementById('map-canvas').getAttribute('data-zoom');
-    // var visuals = document.getElementById('map-canvas').getAttribute('data-visuals');
-    
-    var mapCanvas = document.getElementById('map-canvas');
     var lat = $('#map-canvas').data('lat'); 
     var lng = $('#map-canvas').data('lng');
     var zoom_level = $('#map-canvas').data('zoom');
     var visuals = $('#map-canvas').data('visuals');
 
+    var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
       center: new google.maps.LatLng(lat, lng),
       zoom: zoom_level,
-      mapTypeId: google.maps.MapTypeId.HYBRID
+      mapTypeId: google.maps.MapTypeId.TERRAIN
     };
 
     map = new google.maps.Map(mapCanvas, mapOptions); //global
 
+    function addMarker(visual) {
+      // debugger
+      var image = {
+        url: visual.thumbnail_url,
+        // This marker is 20 pixels wide by 32 pixels tall.
+        size: new google.maps.Size(200, 200),
+        // The origin for this image is 0,0.
+        origin: new google.maps.Point(0,0),
+        // The anchor for this image is the base of the flagpole at 0,32.
+        anchor: new google.maps.Point(0, 80)
+        // radius: new google.maps.Radius(100)
+      };
+
+      var shape = {
+        coords: [1, 1, 1, 20, 18, 20, 18 , 1],
+        type: 'poly'
+      };
+
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(visual.lat, visual.lng),
+        icon: image,
+        map: map,
+        shape: shape,
+        zIndex: visual.id,
+        animation: google.maps.Animation.DROP
+      });
+    };
+
+    for (var i = 0, visual; visual = visuals[i]; i++) {
+      addMarker(visual);
+    };
+
   }
 
-   google.maps.event.addDomListener(window, 'load', initialize); 
-
-
+  google.maps.event.addDomListener(window, 'load', initialize); 
 
 });
 
-function addMarker(visual) {
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(visual.lat, visual.lng),
-    icon: visual.thumbnail_url,
-    map: map
-  });
-
-}
 
 
 
