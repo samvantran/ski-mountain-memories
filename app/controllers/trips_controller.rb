5beb99dab4e0d227cfa981a73a9832e3fe8c0fb9
Jimmy Kuruvilla <jimmyjk@gmail.com>
2014-12-09 00:15:59 -0500

    added notices for new page on errors

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -17,25 +17,23 @@ class TripsController < ApplicationController
 
   def new
     @trip = Trip.new
-    3.times { @trip.users.build }
-  end
+   end
 
   def create
-    @message=""
     if params[:trip][:mountain_id]=="" #no mountain entered
-      @message="Please select a mountain"
+      session[:message]="Please select a mountain"
     else
       trip = Trip.new(mountain_id: params[:trip][:mountain_id], hashtag: params[:trip][:hashtag])
       trip.users << current_user
       if !trip.save #couldn't save.
       # JJK: I think this kind of behavior should be handled in the model and then written to ERRORS, and ERROrs should be in the view
-        @message="Please select another hashtag"
+        session[:message]="Please select another hashtag"
       else
         Instagram.create_subscription("tag", "https://ski-mountain-memories.herokuapp.com/sessions/sub_callback", object_id: trip.hashtag)
         redirect_to trip_path(trip.id) and return
       end
     end
-      redirect_to new_trip_path, notice: @message
+      redirect_to new_trip_path
     end
     
 
